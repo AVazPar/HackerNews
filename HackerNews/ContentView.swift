ec7897d3732afe27ab3dca00c5117f640c5bf408
Ángeles Vázquez Parra <avazpar@gmail.com>
2020-04-23 15:57:17 +0200

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Ángeles Vázquez Parra on 23/04/2020.
+//  Copyright © 2020 Ángeles Vázquez Parra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
